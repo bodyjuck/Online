42e0d720470e870d945ed197b33d4fbc2e0e7499
Nattapat Tapdungkeow <bodyjuck@gmail.com>
2019-05-22 09:41:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -64,12 +64,12 @@ io.on('connection', function (socket) {
 
         if(!gameIsPlaying)
         {
-            if(data.CheckReady == "true")
+            if(data.checkReady == true)
             {
                 numberPlayerReadyInServer++;
 
             }
-            else if(data.CheckReady == "false")
+            else if(data.checkReady == false)
             {
                 numberPlayerReadyInServer--;
             }
